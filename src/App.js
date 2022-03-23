52756034596b345c0297eb62f897c5c24ed88ba4
Farell Elghifari P <farellelghifarip@192.168.1.11>
2022-03-23 16:48:58 +0700

    Homework FE6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './Data/data.js'
+import react from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Cover">
+        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
+      </div>
+      <div className="Container">
+        <h2>{data.name}</h2>
+        <h3>{data.album.name}</h3>
+        <p>{data.artists[0].name}</p>
+	      <button>
+          <a href={data.album.artists[0].external_urls.spotify}>Go to The Song</a>
+        </button>
+      </div>
     </div>
   );
 }
