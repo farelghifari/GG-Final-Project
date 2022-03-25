4f2c884d9ca1724e8161e54683cf6b8808ff84b4
Farell Elghifari P <farellelghifarip@192.168.1.11>
2022-03-25 16:06:36 +0700

    Homework 2/2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
 import data from './Data/data.js'
-import react from "react";
+import Card from './components/Card/index'
 
 function App() {
   return (
     <div className="App">
-      <div className="Cover">
-        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
-      </div>
-      <div className="Container">
-        <h2>{data.name}</h2>
-        <h3>{data.album.name}</h3>
-        <p>{data.artists[0].name}</p>
-	      <button>
-          <a href={data.album.artists[0].external_urls.spotify}>Go to The Song</a>
-        </button>
-      </div>
+      <Card
+       imageUrl={data.album.images[0].url}
+       title={data.album.name}
+       artist={data.artists[0].name}
+       externalUrl={data.album.artists[0].external_urls.spotify}
+      />
     </div>
   );
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+function Card ({imageUrl, title, artist, externalUrl}) {
+    return (
+        <div>
+            <div className="Cover">
+                <img src={imageUrl} width="300" height="300" className="rotate" />
+            </div>
+            <div className="Container">
+                <h2>{title}</h2>
+                <p>{artist}</p>
+                <button>
+                    <a href={externalUrl}>Go to The Song</a>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
